79ff20a477cd9505d055e10081b07646051d375e
Alexander Wollan <59790234+4-W@users.noreply.github.com>
2020-01-26 22:17:07 +0100

    added count for usernames

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import requests
 
 url = "https://api.byte.co/account/register/precheck"
+afile = "available.txt"
+cfile = "usernames.txt"
+
+def count():
+    namecount = 0
+    with open(cfile) as f:
+            for namecount, l in enumerate(f):
+                pass
+            namecount = namecount + 1
+
+
+    return namecount
+    
+def check()
+
 
 def initialize():
     print("Byte Checker by 4W#2100")
     print("The first & the original one")
     print("")
-    print("")
+    print(f"{count()} usernames detected in usernames.txt")
+    print("Press ENTER KEY to begin checking")
+    input("")
+    check()
 data = {"username":"cs"}
 
 r = requests.post(url, json=data)
